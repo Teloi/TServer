2b6f6559012306e18b50f578201352ea22696e6a
teloi <99101154@qq.com>
2020-06-15 16:45:57 +0800

    设置查看请求参数

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './middleware/logger.middleware';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json()); // For parsing application/json
+  app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
   // app.setGlobalPrefix('xxx'); 设置全局路由前缀
   app.use(logger);               // 启用日志
   await app.listen(5859);
