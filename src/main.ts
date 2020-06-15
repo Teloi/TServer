3a8b5d5c3f464221e931515072744870f173659c
teloi <99101154@qq.com>
2020-06-15 16:31:27 +0800

    实体查找

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logger } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.setGlobalPrefix('xxx'); 设置全局路由前缀
+  app.use(logger);               // 启用日志
   await app.listen(5859);
 }
 bootstrap();
