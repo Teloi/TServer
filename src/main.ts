61991a634935182fb2a32b7608314e2ccb5c4591
teloi <99101154@qq.com>
2020-06-15 10:27:46 +0800

    设置配置文件,安装typeorm,mysql,reflect-metadata

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('xxx'); 设置全局路由前缀
   await app.listen(5859);
 }
 bootstrap();
