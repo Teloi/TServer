b227fb2f40cad8937b66dd4ea9eef59a79502e39
teloi <99101154@qq.com>
2020-06-15 16:40:58 +0800

    链接多数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { main, test } from '../src_config/ormconfig';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot(main),
+    TypeOrmModule.forRoot(test),
     RoutesModule
   ],
   controllers: [AppController],
---
+++ src_config/ormconfig.example.ts
@@ -0,0 +1,28 @@
+export const main: any = {
+  "type": "mysql",
+  "host": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "port": 3306,
+  "username": "root",
+  "password": "xxxxxxxxxxxxxxxxxxxxxxxx",
+  "entities": [
+    "dist/src/entity/db-main/**/*.entity.{ts,js}"
+  ],
+  "database": "ts-main",
+  "synchronize": true,
+  "logging": false
+};
+
+export const test: any = {
+  "name": "test",
+  "type": "mysql",
+  "host": "xxxxxxxxxxxxxxxxxxxxxxxx",
+  "port": 3306,
+  "username": "root",
+  "password": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "entities": [
+    "dist/src/entity/db-test/**/*.entity.{ts,js}"
+  ],
+  "database": "ts-test",
+  "synchronize": true,
+  "logging": false
+}
