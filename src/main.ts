3a85b37ce88ea8e344c2b15cb2519980920580d6
teloi <99101154@qq.com>
2020-06-15 17:50:19 +0800

    各种拦截器,过滤器,中间件

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { logger } from './middleware/logger.middleware';
+import { logger } from './core/middleware/logger.middleware';
 import * as express from 'express';
+import { TransformInterceptor } from './core/interceptor/transform.interceptor';
+import { HttpExceptionFilter } from './core/filter/http-exception.filter';
+import { AllExceptionsFilter } from './core/filter/any-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.json()); // For parsing application/json
   app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
-  // app.setGlobalPrefix('xxx'); 设置全局路由前缀
-  app.use(logger);               // 启用日志
+  // 设置全局路由前缀
+  // app.setGlobalPrefix('xxx');
+
+  // 监听所有的请求路由，并打印日志
+  app.use(logger);
+
+  // 使用拦截器打印出参
+  app.useGlobalInterceptors(new TransformInterceptor()); // 启用全局拦截器
+
+  // 过滤处理 HTTP 异常
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  
+  // 启动
   await app.listen(5859);
 }
 bootstrap();
