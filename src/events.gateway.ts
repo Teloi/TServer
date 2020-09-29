ff7cbd3542376de415bbdeae3e051f744e1ce8c8
teloi <99101154@qq.com>
2020-09-29 11:59:13 +0800

    ws demo

--- src/events.gateway.ts
+++ src/events.gateway.ts
@@ -1,9 +1,36 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import * as url from "url"
 
 @WebSocketGateway()
 export class EventsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @WebSocketServer() server;
+
+  private clientsArr: any[] = [];
+
+  handleConnection(client: any,) {
+    console.log('有人链接了' + client.id);
   }
+
+  handleDisconnect(client: any) {
+    console.log('有人走了' + client.id);
+  }
+
+  @SubscribeMessage('addCart')
+  addCart(client: any, payload: any) {
+    console.log(payload)
+    var roomid = url.parse(client.request.url, true).query.roomid;   /*获取房间号 获取桌号*/
+    client.join(roomid);
+    // this.server.to(roomid).emit('addCart','Server AddCart Ok');    //广播所有人包含自己
+    client.broadcast.to(roomid).emit('addCart', 'Server AddCart Ok');   //不包括自己
+  }
+  
 }
+
+  // @SubscribeMessage('events')
+  // handleEvent(@MessageBody() data: string): string {
+  //   return data;
+  // }
+  // @SubscribeMessage('message')
+  // handleMessage(client: any, payload: any): string {
+  //   return 'Hello world!';
+  // }
